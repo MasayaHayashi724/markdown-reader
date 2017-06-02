07c6cc232233b338f9bbd15fd4933668ddf1132a
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-06-02 21:31:07 +0900

    Create view controller for display markdown

---
+++ markdown-reader/MarkdownViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MarkdownViewController.swift
+//  markdown-reader
+//
+//  Created by Masaya Hayashi on 2017/06/02.
+//  Copyright © 2017年 Masaya Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class MarkdownViewController: UIViewController {
+
+    // - Properties
+
+    var content: String = ""
+
+    // - UI
+
+    @IBOutlet private weak var mdTextView: UITextView!
+
+    // - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.mdTextView.text = content
+    }
+
+    func set(title: String, content: String) {
+        self.title = title
+        self.content = content
+    }
+}
